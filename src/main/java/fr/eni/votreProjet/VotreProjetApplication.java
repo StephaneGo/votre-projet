12ab7da4621e8d362391d8293d649743af5650c9
StephCAMPUS <sgobin@campus-eni.fr>
2023-07-20 10:51:57 +0200

    Creation du projet

---
+++ src/main/java/fr/eni/votreProjet/VotreProjetApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.votreProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotreProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotreProjetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/votreProjet/VotreProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.votreProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotreProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
